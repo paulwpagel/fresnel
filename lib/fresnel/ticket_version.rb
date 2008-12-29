5059d290a54d98b71cf6726029ac1d1da27b3a14
Eric Meyer <ericmeyer@localhost.(none)>
2008-12-29 16:21:43 -0600

    moving ticket version

---
+++ lib/fresnel/ticket_version.rb
@@ -0,0 +1,31 @@
+require File.expand_path(File.dirname(__FILE__) + "/../../vendor/lighthouse-api/lib/lighthouse")
+
+module Fresnel
+  class TicketVersion
+  
+    def initialize(lighthouse_version)
+      @lighthouse_version = lighthouse_version
+    end
+  
+    def comment
+      return @lighthouse_version.body
+    end
+  
+    def timestamp
+      return @lighthouse_version.updated_at
+    end
+  
+    def created_by
+      return user.name if user
+      return ""
+    end
+  
+    def user
+      begin
+        return Lighthouse::User.find(@lighthouse_version.user_id)
+      rescue ActiveResource::ResourceNotFound => e
+        return nil
+      end
+    end
+  end
+end
