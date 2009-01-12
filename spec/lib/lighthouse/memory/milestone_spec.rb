79b3a61f900a0dafe7a5b3d5d37cf539e7b3b8f5
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-12 12:05:48 -0600

    got rid of old in memory files

--- spec/lib/lighthouse/memory/milestone_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../../spec_helper")
-
-describe Lighthouse::Memory::Milestone do
-
-  it "should have title" do
-    milestone = Lighthouse::Memory::Milestone.new(:title => "The great milestone", :id => 2)
-    milestone.title.should == "The great milestone"
-  end
-end
