489114f2c10e7c3faf61d06572fd3af9ff7aa34b
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-13 11:22:53 -0600

    pulled base methods out

--- lib/lighthouse/lighthouse.rb
+++ lib/lighthouse/lighthouse.rb
@@ -1,18 +1,10 @@
 require 'activeresource'
 require "lighthouse/memory/project"
+require "lighthouse/memory/project_membership"
 require "lighthouse/memory/user"
 require "lighthouse/memory/ticket"
 require "lighthouse/memory/milestone"
-
-module Lighthouse
-  
-  def self.account=(value)
-  end
-  
-  def self.authenticate(username, password)
-    return true
-  end
-end
+require "lighthouse/memory/base"
 
 project = Lighthouse::Project.new(:name => "fresnel")
 Lighthouse::User.new(:name => "Marion Morison", :id => rand(1000))
