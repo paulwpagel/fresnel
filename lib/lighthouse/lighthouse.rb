4f0f954363eb299da4e6b61d39ed6e648fc3dac9
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-13 11:19:25 -0600

    pulled out project membership and milestone

--- lib/lighthouse/lighthouse.rb
+++ lib/lighthouse/lighthouse.rb
@@ -2,6 +2,7 @@ require 'activeresource'
 require "lighthouse/memory/project"
 require "lighthouse/memory/user"
 require "lighthouse/memory/ticket"
+require "lighthouse/memory/milestone"
 
 module Lighthouse
   
@@ -10,64 +11,6 @@ module Lighthouse
   
   def self.authenticate(username, password)
     return true
-  end
-    
-  class Milestone
-    @@milestones = []
-    
-    def self.destroy_all
-      @@milestones = []
-    end
-    
-    def self.find(param1, param2)
-      project_id = param2[:params][:project_id]
-      return @@milestones.find_all { |milestone| milestone.project_id == project_id }
-    end
-    
-    attr_reader :project_id, :title
-    
-    def initialize(options = {})
-      @project_id = options[:project_id]
-      @title = options[:title]
-    end
-    
-    def save
-      unless @id
-        @id = rand 10000
-        @@milestones << self
-      end
-    end
-  end
-  
-  class ProjectMembership
-    @@project_memberships = []
-    
-    class << self
-      def find(*params)
-        project_id = params[1][:params][:project_id]
-        return @@project_memberships.find_all {|membership| membership.project_id == project_id}
-      end
-    
-      def destroy_all
-        @@project_memberships = []
-      end
-      
-      def all_users_for_project(project_id)
-        return []
-      end
-    end
-    
-    
-    attr_reader :project_id
-    
-    def initialize(options = {})
-      @project_id = options[:project_id]
-    end
-    
-    def save
-      @@project_memberships << self
-    end
-    
   end
 end
 
---
+++ lib/lighthouse/memory/milestone.rb
@@ -0,0 +1,28 @@
+module Lighthouse
+  class Milestone
+    @@milestones = []
+    
+    def self.destroy_all
+      @@milestones = []
+    end
+    
+    def self.find(param1, param2)
+      project_id = param2[:params][:project_id]
+      return @@milestones.find_all { |milestone| milestone.project_id == project_id }
+    end
+    
+    attr_reader :project_id, :title
+    
+    def initialize(options = {})
+      @project_id = options[:project_id]
+      @title = options[:title]
+    end
+    
+    def save
+      unless @id
+        @id = rand 10000
+        @@milestones << self
+      end
+    end
+  end
+end
