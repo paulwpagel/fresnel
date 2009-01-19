b9b9d02d2a8a7ab7083953fc515b976816a95103
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-19 11:09:04 -0600

    first test on ticket sorter

--- production/list_tickets/players/ticket_sorter.rb
+++ production/list_tickets/players/ticket_sorter.rb
@@ -1,2 +1,11 @@
 module TicketSorter
+  def mouse_clicked(event)
+    scene.ticket_master.get_tickets(type)
+  end
+  
+  private
+  
+  def type
+    scene.find("ticket_type").value
+  end
 end
--- spec/list_tickets/ticket_sorter_spec.rb
+++ spec/list_tickets/ticket_sorter_spec.rb
@@ -1,8 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+require 'limelight/specs/spec_helper'
 require "ticket_sorter"
 
 describe TicketSorter do
-  it "should exist" do
+
+  before(:each) do
+    @mock_master = mock(TicketMaster, :show_tickets => nil)
+    TicketMaster.stub!(:new).and_return(@mock_master)
+  end
+  
+  uses_scene :list_tickets
+  
+  it "should get the appropriate tickets from the ticket master" do
+    scene.find("ticket_type").value = "Open Tickets"
     
+    @mock_master.should_receive(:get_tickets).with("Open Tickets")
+    
+    scene.find("title_header").mouse_clicked(@nil)
   end
 end
