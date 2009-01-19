f8ed760f9cce2d2063ea4663a7ed4a904bc9531b
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-19 10:11:06 -0600

    adjusting styles for ticket list

--- production/list_tickets/props.rb
+++ production/list_tickets/props.rb
@@ -2,6 +2,6 @@ main {
   __install "common/title_bar.rb"
   row {
     ticket_lister :id => "ticket_lister"
-    combo_box :id => "ticket_type", :players => "type_selector", :choices => ["Open Tickets", "All Tickets"]
+    combo_box :id => "ticket_type", :players => "type_selector", :choices => ["Open Tickets", "All Tickets"], :width => 150
   }
 }
--- production/list_tickets/styles.rb
+++ production/list_tickets/styles.rb
@@ -5,14 +5,13 @@ row {
 
 ticket_in_list {
   padding 10
-  rounded_corner_radius "10"
-  border_width 4
+  width "90%"
+  border_width 2
   border_color "006699"
   background_color "5A9ECF"
-  width 500
-  height 50
   hover {
-      border_width 4
+      border_width 2
       border_color "A63101"
+      background_color "FFFFFF"
     }
 }
