a3d1ba5d5e071459900b8e019a55fcfed8636d68
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-19 11:19:56 -0600

    ticket sorter will sort by title

--- production/list_tickets/players/ticket_sorter.rb
+++ production/list_tickets/players/ticket_sorter.rb
@@ -1,11 +1,16 @@
 module TicketSorter
   def mouse_clicked(event)
-    scene.ticket_master.get_tickets(type)
+    scene.ticket_lister.show_these_tickets(sorted_tickets)
   end
   
   private
   
   def type
     scene.find("ticket_type").value
+  end
+  
+  def sorted_tickets
+    tickets = scene.ticket_master.get_tickets(type)
+    sorted_tickets = tickets.sort_by { |ticket| ticket.title }
   end
 end
--- spec/list_tickets/ticket_sorter_spec.rb
+++ spec/list_tickets/ticket_sorter_spec.rb
@@ -5,7 +5,11 @@ require "ticket_sorter"
 describe TicketSorter do
 
   before(:each) do
-    @mock_master = mock(TicketMaster, :show_tickets => nil)
+    @first = ticket("a")
+    @second = ticket("b")
+    @third = ticket("c")
+    @tickets = [@second, @third, @first]
+    @mock_master = mock(TicketMaster, :show_tickets => nil, :get_tickets => @tickets)
     TicketMaster.stub!(:new).and_return(@mock_master)
   end
   
@@ -14,8 +18,18 @@ describe TicketSorter do
   it "should get the appropriate tickets from the ticket master" do
     scene.find("ticket_type").value = "Open Tickets"
     
-    @mock_master.should_receive(:get_tickets).with("Open Tickets")
+    @mock_master.should_receive(:get_tickets).with("Open Tickets").and_return([])
     
     scene.find("title_header").mouse_clicked(@nil)
+  end
+  
+  it "should list the tickets by title" do
+    scene.ticket_lister.should_receive(:show_these_tickets).with([@first, @second, @third])
+    
+    scene.find("title_header").mouse_clicked(@nil)
+  end
+  
+  def ticket(title)
+    return mock("ticket", :title => title)
   end
 end
