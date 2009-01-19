17d43a9c37de18b5c74e4b5a0df3ffcc55599858
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-19 12:03:40 -0600

    toggle between asc and desc

--- production/list_tickets/players/ticket_sorter.rb
+++ production/list_tickets/players/ticket_sorter.rb
@@ -1,6 +1,7 @@
 module TicketSorter
   def mouse_clicked(event)
     scene.ticket_lister.show_these_tickets(sorted_tickets)
+    toggle_sort_order
   end
   
   private
@@ -11,6 +12,16 @@ module TicketSorter
   
   def sorted_tickets
     tickets = scene.ticket_master.get_tickets(type)
-    sorted_tickets = tickets.sort_by { |ticket| ticket.title.downcase }
+    ascending_tickets = tickets.sort_by { |ticket| ticket.title.downcase }
+    return ascending_tickets.reverse if production.current_sort_order == "ascending"
+    return ascending_tickets
+  end
+  
+  def toggle_sort_order
+    if production.current_sort_order == "ascending"
+      production.current_sort_order = "descending"
+    else
+      production.current_sort_order = "ascending"
+    end
   end
 end
--- spec/list_tickets/ticket_sorter_spec.rb
+++ spec/list_tickets/ticket_sorter_spec.rb
@@ -14,6 +14,11 @@ describe TicketSorter do
   end
   
   uses_scene :list_tickets
+
+  before(:each) do
+    scene.production.current_sort_order = nil
+    scene.ticket_lister.stub!(:show_these_tickets)
+  end
   
   it "should get the appropriate tickets from the ticket master" do
     scene.find("ticket_type").value = "Open Tickets"
@@ -40,6 +45,29 @@ describe TicketSorter do
     sort_by_title
   end
   
+  it "should keep track of ascending vs descending" do
+    sort_by_title
+    
+    scene.production.current_sort_order.should == "ascending"
+  end
+  
+  describe "current_sort_order is ascending" do
+    before(:each) do
+      scene.production.current_sort_order = "ascending"
+    end
+    
+    it "should the current order is ascending, it should sort descending" do
+      scene.ticket_lister.should_receive(:show_these_tickets).with([@third, @second, @first])
+    
+      sort_by_title
+    end
+  
+    it "should toggle the current_sort_order" do
+      sort_by_title
+    
+      scene.production.current_sort_order.should == "descending"
+    end
+  end
   def ticket(title)
     return mock("ticket #{title}", :title => title)
   end
