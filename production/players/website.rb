d8a55a93841a699c08c089eae99ec5b4631cabdd
Eric Meyer <ericmeyer@localhost.(none)>
2009-01-27 11:02:22 -0600

    account is in url

--- production/players/website.rb
+++ production/players/website.rb
@@ -2,6 +2,16 @@ import "src.Browser"
 
 module Website
   def button_pressed(event)
-    Browser.new.showInBrowser("http://lighthouseapp.com")
+    Browser.new.showInBrowser(url)
+  end
+  
+  def url
+    return "http://#{account}.lighthouseapp.com"
+  end
+  
+  private
+  
+  def account
+    return production.lighthouse_client.account
   end
 end
