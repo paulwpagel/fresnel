b13509b5d01678677d225914f87a010722ed769f
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-05 11:36:47 -0600

    fixed a bug with UnauthorizedAccess for finding project memberships

--- limelight/production/__resources/gems/fresnel_lib-0.0.12/lib/lighthouse/lighthouse_api/project_membership.rb
+++ limelight/production/__resources/gems/fresnel_lib-0.0.12/lib/lighthouse/lighthouse_api/project_membership.rb
@@ -4,7 +4,11 @@ module Lighthouse
   module LighthouseApi
     class ProjectMembership
       def self.all_users_for_project(project_id)
-        memberships = Lighthouse::ProjectMembership.find(:all, :params => {:project_id => project_id})
+        begin
+          memberships = Lighthouse::ProjectMembership.find(:all, :params => {:project_id => project_id})
+        rescue  ActiveResource::UnauthorizedAccess
+          return []
+        end
         return memberships.collect{|membership| User.find_by_id(membership.user_id)}
       end
     end
