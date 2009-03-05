1acfde50b59276cddd5d387e8a962924b3c7b082
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-05 16:04:30 -0600

    moved clear tag filter to ticket lister. deleted unused code

--- limelight/production/list_tickets/players/all_tags.rb
+++ limelight/production/list_tickets/players/all_tags.rb
@@ -1,6 +1,6 @@
 module AllTags
   def mouse_clicked(event)
-    scene.ticket_master.clear_tag_filter
+    scene.ticket_lister.clear_tag_filter
     scene.tag_lister.show_project_tags
   end
 end
--- limelight/production/list_tickets/stagehands/ticket_master.rb
+++ limelight/production/list_tickets/stagehands/ticket_master.rb
@@ -4,15 +4,6 @@ class TicketMaster
     @scene = scene
   end
   
-  def filter_by_type(type)
-    @current_type_filter = type
-    if @current_tag_filter
-      filter_by_type_and_tag
-    else
-      @scene.ticket_lister.show_these_tickets(tickets_for_type(type)) if @scene.ticket_lister
-    end
-  end
-
   def tickets_for_type_and_tag(type, tag)
     return tickets_for_type(type) if tag.nil?
     return project.tickets_for_tag(tag) if type.nil?
@@ -22,11 +13,6 @@ class TicketMaster
   def filter_by_type_and_tag
     tickets = project.tickets_for_tag(@current_tag_filter) & tickets_for_type(@current_type_filter)
     @scene.ticket_lister.show_these_tickets(tickets) if @scene.ticket_lister
-  end
-  
-  def clear_tag_filter
-    @current_tag_filter = nil
-    filter_by_type(@current_type_filter)
   end
 
   #TODO - EWM pull this method out
