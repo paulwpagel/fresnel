dabe1ccc6a76aa737ee07a1f7fa5b4f7a1aa8aab
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-25 14:56:51 -0500

    milestone creation works with title only so far

--- limelight/production/milestones/players/create_milestone.rb
+++ limelight/production/milestones/players/create_milestone.rb
@@ -1,6 +1,10 @@
 module CreateMilestone
   prop_reader :new_milestone_title
   
+  def button_pressed(event)
+    show_spinner { create }
+  end
+  
   def create
     current_project.create_milestone(:title => new_milestone_title.text)
   end
