ad5f4dcfad2e60ae2e805fd036f2a99bcd1c8a1c
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-25 15:28:04 -0500

    added input fields to milestone form

--- limelight/production/milestones/props.rb
+++ limelight/production/milestones/props.rb
@@ -1,8 +1,17 @@
 main {
+  button :id => "back_button", :text => "View Tickets", :on_button_pressed => "scene.load('list_tickets')"
   create_milestone_form {
     input_group {
       label :text => "Title"
       text_box :id => "new_milestone_title"
+    }
+    input_group {
+      label :text => "Due Date (MM-DD-YYYY)"
+      text_box :id => "new_milestone_due_on"
+    }
+    input_group {
+      label :text => "Goals"
+      text_area :id => "new_milestone_goals"
     }
     button :id => "create_milestone", :text => "Create Milestone", :players => "create_milestone", :width => 200
   }
--- limelight/prop_spec/milestones_props_spec.rb
+++ limelight/prop_spec/milestones_props_spec.rb
@@ -3,18 +3,36 @@ require 'limelight/specs/spec_helper'
 
 describe "Milestones Props" do
   before(:each) do
+    setup_mocks
   end
   
   uses_scene :milestones
   
-  it "should an input for the title" do
+  it "should have an input for the title" do
     prop = scene.find("new_milestone_title")
     prop.name.should == "text_box"
   end
   
+  it "should have an input for the goals" do
+    prop = scene.find("new_milestone_goals")
+    prop.name.should == "text_area"
+  end
+
+  it "should have an input for the due date" do
+    prop = scene.find("new_milestone_due_on")
+    prop.name.should == "text_box"
+  end
+
   it "should have a button to create the milestone" do
     prop = scene.find("create_milestone")
     prop.name.should == "button"
     prop.players.should == "create_milestone"
+  end
+  
+  it "should a link back to list_tickets" do
+    scene.should_receive(:load).with('list_tickets')
+    prop = scene.find("back_button")
+
+    prop.button_pressed(nil)
   end
 end
