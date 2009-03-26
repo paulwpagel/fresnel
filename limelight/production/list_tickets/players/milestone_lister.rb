bfeeeb165d6545aa6ac9346ab4053d68da1d9def
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-26 11:03:07 -0500

    work on milestone lister. need to use it

--- limelight/production/list_tickets/players/milestone_lister.rb
+++ limelight/production/list_tickets/players/milestone_lister.rb
@@ -1,5 +1,30 @@
 module MilestoneLister
-  def list_titles
-    production.stage_manager[scene.stage.name]
+  def show_project_milestones
+    remove_all
+    list_titles
+  end
+  
+  def activate(id)
+    milestone = scene.find(id)
+    remove_all
+    list_titles milestone.text
+  end
+  
+  def list_titles(active_milestone_text = nil)
+    project.milestone_titles.each_with_index do |milestone_text, index|
+      name = "tag"
+      name = "active_milestone" if active_milestone_text == milestone_text
+      add(Limelight::Prop.new(:name => name, :text => milestone_text, :id => "milestone_#{index + 1}"))
+    end
+  end
+  
+  private ####################################
+  
+  def project
+    return production.stage_manager[stage_name].current_project
+  end
+  
+  def stage_name
+    return scene.stage.name
   end
 end
--- limelight/spec/list_tickets/milestone_lister_spec.rb
+++ limelight/spec/list_tickets/milestone_lister_spec.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 require "milestone_lister"
 
+module Limelight
+  class Prop
+  end
+end
+
 describe MilestoneLister do
   before(:each) do
     mock_stage_manager
@@ -8,6 +13,7 @@ describe MilestoneLister do
     @milestone_lister, @scene, @production = create_player(MilestoneLister, 
                                                 :scene => {:stage => @stage}, 
                                                 :production => {:stage_manager => @stage_manager})
+    @milestone_lister.stub!(:remove_all)
   end
   
   it "should get the current project from the stage_manager using the stage name" do
@@ -15,4 +21,53 @@ describe MilestoneLister do
     
     @milestone_lister.list_titles
   end
+  
+  it "should make a prop for one milestone title" do
+    prop = mock('prop')
+    @project.should_receive(:milestone_titles).and_return(["Milestone One"])
+    Limelight::Prop.should_receive(:new).with(:name => "tag", :text => "Milestone One", :id => "milestone_1").and_return(prop)
+    @milestone_lister.should_receive(:add).with(prop)
+    
+    @milestone_lister.list_titles
+  end
+  
+  it "should make a prop for the second milestone title" do
+    prop = mock('prop')
+    prop2 = mock('prop2')
+    @project.should_receive(:milestone_titles).and_return(["Milestone One", "Milestone Two"])
+    Limelight::Prop.should_receive(:new).with(:name => "tag", :text => "Milestone One", :id => "milestone_1").and_return(prop)
+    Limelight::Prop.should_receive(:new).with(:name => "tag", :text => "Milestone Two", :id => "milestone_2").and_return(prop2)
+    @milestone_lister.should_receive(:add).with(prop)
+    @milestone_lister.should_receive(:add).with(prop2)
+    
+    @milestone_lister.list_titles
+  end
+
+  it "sets a milestone title to be active" do
+    prop = mock('prop')
+    @project.should_receive(:milestone_titles).and_return(["Milestone One"])
+    active_prop = mock('active prop')
+    active_prop.should_receive(:text).and_return("Milestone One")
+    @scene.should_receive(:find).with("id").and_return(active_prop)
+    Limelight::Prop.should_receive(:new).with(:name => "active_milestone", :text => "Milestone One", :id => "milestone_1").and_return(prop)
+    @milestone_lister.should_receive(:add).with(prop)
+    @milestone_lister.activate("id")
+  end
+  
+  it "should remove_all before it lists" do
+    @milestone_lister.should_receive(:remove_all)
+    @milestone_lister.stub!(:list_titles)
+    
+    @milestone_lister.show_project_milestones
+  end
+  
+  it "should remove_all before it activates a milestone title" do
+    milestone = mock("milestone", :text => nil)
+    @scene.stub!(:find).and_return(milestone)
+    @milestone_lister.should_receive(:remove_all)
+    @milestone_lister.stub!(:list_titles)
+    
+    @milestone_lister.activate("id")
+  end
+  
 end
