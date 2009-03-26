2aab145b2d6ad31d486fa212f4eed5011b866e31
Eric Meyer <ericmeyer@localhost.(none)>
2009-03-26 13:22:30 -0500

    passing in the milestones to the configure milestones props

--- limelight/production/list_tickets/players/configure_milestones.rb
+++ limelight/production/list_tickets/players/configure_milestones.rb
@@ -4,8 +4,14 @@ module ConfigureMilestones
   end
   
   def open_milestones
-    scene.build do
+    scene.build(:milestones => current_project.milestones) do
       __install "list_tickets/configure_milestones_props.rb"
     end
+  end
+  
+  private #####################
+  
+  def current_project
+    production.stage_manager[scene.stage.name].current_project
   end
 end
--- limelight/spec/list_tickets/configure_milestones_spec.rb
+++ limelight/spec/list_tickets/configure_milestones_spec.rb
@@ -5,12 +5,20 @@ describe ConfigureMilestones do
   before(:each) do
     mock_stage_manager
     @configure_milestones, @scene, @production = create_player(ConfigureMilestones,
-                                                :scene => {:build => nil}, 
-                                                :production => {})
+                                                :scene => {:build => nil, :stage => @stage}, 
+                                                :production => {:stage_manager => @stage_manager})
+    @milestones = [mock("milestone")]
+    @project.stub!(:milestones).and_return(@milestones)
+  end
+  
+  it "should use the stage name to get the stage_info" do
+    @stage_manager.should_receive(:[]).with("stage name").and_return(@stage_info)
+    
+    @configure_milestones.open_milestones
   end
   
   it "should display the milestone configuration page" do
-    @scene.should_receive(:build)
+    @scene.should_receive(:build).with(:milestones => @milestones)
     
     @configure_milestones.open_milestones
   end
